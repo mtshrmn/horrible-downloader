02fe56780b7358d8ed9043e1d207ed6aae242fd2
Jelomite <moshesher1998@gmail.com>
2018-06-22 06:02:08 +0300

    config controlled parser with error handling

--- parser.py
+++ parser.py
@@ -1,32 +1,86 @@
 import re
 import os
+import sys
 import subprocess
 import feedparser
 from bs4 import BeautifulSoup
 import argparse
 import requests
-animes = ['alice or alice', 'steins gate 0']
+
+subprocess.call(['clear'])
+
+with open('config', mode='r') as f:
+    text = iter(f.read().split('\n'))
+    parsed_config = {}
+
+    while True:
+        try:
+            line = next(text)
+            if line.startswith('#'):
+                continue
+            if line == 'resolution:':
+                parsed_config['quality'] = next(text).strip(' ')
+
+            if line == 'download_dir:':
+                parsed_config['path'] = next(text).strip(' ')
+
+            if line == 'anime:':
+                parsed_config['anime'] = []
+                while True:
+                    show = next(text).strip(' ')
+                    if show:
+                        parsed_config['anime'].append(show)
+        except StopIteration:
+            break
+
 url = 'http://horriblesubs.info/current-season/'
-html = requests.get(url).text
+print('fetching feed...')
+
+try:
+    html = requests.get(url).text
+except requests.exceptions.RequestException as e:
+    print(e)
+    sys.exit(1)
 
 soup = BeautifulSoup(html, 'lxml')
-items = soup.find_all(name='div', attrs={'class': 'ind-show linkful'})
 current_season = []
-for item in items:
-    current_season.append(item.a.string.lower())
-print('fetching feed...')
+
+for anime in soup.find_all(name='div', attrs={'class': 'ind-show linkful'}):
+    current_season.append(anime.a.string.lower())
+
+for anime in parsed_config['anime']:
+    if anime not in current_season:
+        print(
+            f'"{anime}" isnt in the current season shows, please check for correctness')
+        sys.exit(1)
+
 anime_feed = feedparser.parse("http://horriblesubs.info/rss.php?res=all")
 
 links = []
-for anime in animes:
-    if anime not in current_season:
-        print(f'"{anime}" isnt in the current season shows, please check for correctness')
-        break;
-    r = re.compile(r'\[HorribleSubs\] {} - \d+ \[1080p\]\.mkv'.format(anime), flags=re.IGNORECASE)
+quality = parsed_config['quality']
+for anime in parsed_config['anime']:
+    r = re.compile(
+        r'\[HorribleSubs\] {} - \d+ \[{}p\]\.mkv'.format(anime, quality), flags=re.IGNORECASE)
     for entry in anime_feed.entries:
         if r.match(str(entry.title)):
-            print(entry.title)
-            links.append(entry.link)
-path = os.path.join('downloads')
+            links.append({
+                'title': entry.title,
+                'date': entry.published_parsed,
+                'magnet': entry.link
+            })
+subprocess.call(['clear'])
+print('found new episodes to download: \n')
 for link in links:
-    subprocess.call(['webtorrent', link, '-o', path])
+    print(link['title'])
+
+inp = input('\nwould you like to proceed? [Y/n] ')
+if inp not in ('', 'Y', 'y', 'yes', 'Yes'):
+    print('aborting download')
+    sys.exit(1)
+
+path = os.path.expanduser(parsed_config['path'])
+
+for link in links:
+    subprocess.call(['webtorrent', link['magnet'], '-o', path])
+
+sys.exit(0)
