c58cfe847f3f4c328eb5d4bb9d9bd52375ac08a8
Jelomite <moshesher1998@gmail.com>
2018-06-23 22:40:03 +0300

    refactored everything

---
+++ horrible-downloader.py
@@ -0,0 +1,54 @@
+import os
+import sys
+from parser import *
+from subprocess import call
+
+call(['clear'])
+
+# set some variables:
+dir = '/'.join(sys.argv[0].split('/')[:-1])
+config = parse_conf(dir, 'conf.ini')
+quality = config['settings']['resolution']
+download_dir = config['settings']['download_dir']
+subscriptions = config['subscriptions']
+
+# check integrity of quality
+if quality not in ('480', '720', '1080'):
+    print(f'{quality} isn\'t a valid option. please choose from [480|720|1080]')
+    sys.exit(1)
+
+# get the currently airing shows:
+print('fetching feed...')
+airing = list(get_current_shows())
+
+to_download = []
+for show in subscriptions:
+    # check integrity of subscriptions
+    if show not in airing:
+        print(f'"{show.title()}" doesn\'t currently air. please fix this in the configuration file')
+        sys.exit(1)
+    last_episode_watched = int(subscriptions[show])
+    episodes = list(get_episodes(show))
+    filter_key = lambda e: int(e['episode']) > last_episode_watched and quality in e['full name']
+    to_download += [episode for episode in filter(filter_key, episodes)]
+
+call(['clear'])
+
+print(f'Found {len(to_download)} episodes to download:\n\n')
+for episode in to_download:
+    print(f'{episode["full name"]}.mkv')
+
+inp = input('\n\nwould you like to proceed? [Y/n] ')
+if inp not in ('', 'Y', 'y', 'yes', 'Yes'):
+    print('aborting download\n')
+    sys.exit(1)
+
+# let the downloads begin!
+path = os.path.expanduser(download_dir)
+print(f'Beginning download to {path}')
+for show in reversed(to_download):
+    subdir = os.path.join(path, show['name'].title())
+    call(['webtorrent', show['magnet'], '-o', subdir])
+    config['subscriptions'][show] = show['episode']
+    with open(os.path.join(dir, file), 'w') as f:
+        config.write(f)
--- parser.py
+++ parser.py
@@ -1,136 +1,69 @@
-import re
-import os
-import sys
-import subprocess
-import feedparser
-from bs4 import BeautifulSoup
-import requests
-import json
 
-subprocess.call(['clear'])
 
-with open('config', mode='r') as f:
-    text = iter(f.read().split('\n'))
-    parsed_config = {}
+def parse_conf(dir: str, file: str):
+    import os
+    from configparser import ConfigParser
 
-    while True:
-        try:
-            line = next(text)
-            if line.startswith('#'):
-                continue
-            if line == 'resolution:':
-                parsed_config['quality'] = next(text).strip(' ')
+    conf = ConfigParser()
+    success = conf.read(os.path.join(dir, file))
+    if not success:
+        print('No config file found, Generating from default')
 
-            if line == 'download_dir:':
-                parsed_config['path'] = next(text).strip(' ')
+        conf['settings'] = {
+            'resolution': '1080',
+            'download_dir': '~/Videos'
+        }
 
-            if line == 'anime:':
-                parsed_config['anime'] = []
-                while True:
-                    show = next(text).strip(' ')
-                    if show:
-                        parsed_config['anime'].append(show)
-        except StopIteration:
-            break
+        conf['subscriptions'] = {
+            'boku no hero academia': 0,
+            'darling in the franxx': 0,
+            'steins gate 0': 0,
+            'megalo box': 0
+        }
 
-with open('watch.json', mode='r') as f:
-    last_episode = json.load(f)
+        with open(os.path.join(dir, file), 'w') as f:
+            conf.write(f)
 
-for show in parsed_config['anime']:
-    if show not in last_episode.keys():
-        i = input(f'"{show}" does not appear to be in the history file, would you like to add it? [Y/n]')
-        if i not in ('', 'Y', 'y', 'yes', 'Yes'):
-            print('please manualy adjust the config file')
-            sys.exit(1)
+    return conf
 
-        last_episode[show] = 0
 
-url = 'http://horriblesubs.info/current-season/'
-print('fetching feed...')
+def get_current_shows():
+    from bs4 import BeautifulSoup
+    import requests
 
-try:
-    html = requests.get(url).text
-except requests.exceptions.RequestException as e:
-    print(e)
-    sys.exit(1)
+    url = 'http://horriblesubs.info/current-season/'
 
-soup = BeautifulSoup(html, 'lxml')
-current_season = []
-
-for anime in soup.find_all(name='div', attrs={'class': 'ind-show linkful'}):
-    current_season.append(anime.a.string.lower())
-
-for anime in parsed_config['anime']:
-    if anime not in current_season:
-        print(f'"{anime}" is not in the current season shows, please spell check the config file.')
+    try:
+        html = requests.get(url).text
+    except requests.exceptions.RequestException as e:
+        print(e)
         sys.exit(1)
 
-anime_feed = feedparser.parse("http://horriblesubs.info/rss.php?res=all")
+    soup = BeautifulSoup(html, 'lxml')
 
-links = []
-tmp_links = []
-quality = parsed_config['quality']
-for anime in parsed_config['anime']:
-    r = re.compile(r'\[HorribleSubs\] {} - \d+ \[{}p\]\.mkv'.format(anime, quality), flags=re.IGNORECASE)
-    for entry in anime_feed.entries:
-        if r.match(str(entry.title)):
-            current_episode = entry.title.lower().strip(f'[horriblesubs] {anime} - ').replace(f'[{quality}p].mkv', '')
-            links.append({
-                'title': entry.title,
-                'episode': current_episode,
-                'magnet': entry.link
-            })
-        else:
-            current_episode = 1000
-            for e in anime_feed.entries:
-                if r.match(str(e.title)):
-                    current_episode = e.title.lower().strip(f'[horriblesubs] {anime} - ').replace(f'[{quality}p].mkv', '')
-        entry_title = entry.title.lower().replace('[horriblesubs] ', '')
-        entry_title = entry_title.replace(f'[{quality}p].mkv', '')
-        entry_title = ' - '.join(entry_title.split(' - ')[:-1])
-        if int(current_episode) > int(last_episode[anime]) and entry_title in parsed_config['anime']:
-            search_url = "http://horriblesubs.info/lib/search.php?value=" + anime.replace(' ', '-')
-            search_html = requests.get(search_url).text
-            tmp_soup = BeautifulSoup(search_html, 'lxml')
+    for anime in soup.find_all(name='div', attrs={'class': 'ind-show linkful'}):
+        yield anime.a.string.lower()
 
-            matches = tmp_soup.find_all(name='a', attrs={'title': 'Magnet Link'})
 
-            for match in reversed(matches):
-                element = match.parent.parent.parent
-                title = element.text.replace('MagnetTorrentULFUUP', '')
-                stripped = title.lower().strip(anime + '- ')
-                if quality in stripped:
-                    episode = stripped.replace(f'[{quality}p]', '')
-                    try:
-                        if int(last_episode[anime]) < int(episode) < int(current_episode):
-                            tmp_links.append({
-                                'title': f'[HorribleSubs] {title}.mkv',
-                                'episode': episode,
-                                'magnet': match['href']
-                            })
-                    except ValueError:
-                        pass
-            else:
-                last_episode[anime] = episode
-links += tmp_links
+def get_episodes(anime: str):
+    import re
+    import requests
+    from bs4 import BeautifulSoup
+    url = "http://horriblesubs.info/lib/search.php?value=" + \
+        anime.replace(' ', '-')
+    html = requests.get(url).text
+    soup = BeautifulSoup(html, 'lxml')
+    matches = soup.find_all(name='a', attrs={'title': 'Magnet Link'})
 
-# subprocess.call(['clear'])
-print('found new episodes to download: \n')
-
-for link in sorted(links, key=lambda l: l['title'], reverse=True):
-    print(link['title'])
-
-inp = input('\nwould you like to proceed? [Y/n] ')
-if inp not in ('', 'Y', 'y', 'yes', 'Yes'):
-    print('aborting download')
-    sys.exit(1)
-
-with open('watch.json', mode='w') as f:
-    json.dump(last_episode, f)
-
-path = os.path.expanduser(parsed_config['path'])
-
-for link in links:
-    subprocess.call(['webtorrent', link['magnet'], '-o', path])
-
-sys.exit(0)
+    for match in matches:
+        element = element = match.parent.parent.parent
+        title = element.text.replace('MagnetTorrentULFUUP', '')
+        try:
+            yield {
+                'full name': title,
+                'name': anime,
+                'episode': re.search(r'- (\d+) \[', title.lower()).group(1),
+                'magnet': match['href']
+            }
+        except AttributeError:
+            pass
