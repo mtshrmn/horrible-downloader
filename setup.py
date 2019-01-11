3a3906099cbc33c23f14db73a5f916b59eda0b71
suerflowz <moshesher1998@gmail.com>
2019-01-11 16:28:09 +0200

    update to version 0.1.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", 'r') as f:
 
 setup(
     name='Horrible-Downloader',
-    version='0.1.2',
+    version='0.1.3',
     packages=['HorribleDownloader'],
     url='https://github.com/Jelomite/horrible-downloader',
     license='MIT',
