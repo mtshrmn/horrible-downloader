4d273bdca4d43c06c1f0cd1ffbe85380eddb6263
suerflowz <moshesher1998@gmail.com>
2019-02-23 12:54:27 +0200

    update to version 0.1.5

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ class custom_install(install):
 
 setup(
     name='Horrible-Downloader',
-    version='0.1.4',
+    version='0.1.5',
     packages=['HorribleDownloader'],
     url='https://github.com/Jelomite/horrible-downloader',
     license='MIT',
