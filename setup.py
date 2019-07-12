0caa3010f9cb8481c12480ced694e1195102917c
suerflowz <moshesher1998@gmail.com>
2019-07-12 18:03:16 +0300

    bump to version 0.1.6

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ class custom_install(install):
 
 setup(
     name='Horrible-Downloader',
-    version='0.1.5',
+    version='0.1.6',
     packages=['HorribleDownloader'],
     url='https://github.com/Jelomite/horrible-downloader',
     license='MIT',
